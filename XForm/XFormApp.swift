fd832ed1f9f2beb0eed225c7627f92295ca79334
Jonathan Wight <jwight@mac.com>
2024-06-11 22:51:34 -0700

    Initial Commit

---
+++ XForm/XFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  XFormApp.swift
+//  XForm
+//
+//  Created by Jonathan Wight on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct XFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XFormTests/XFormTests.swift
@@ -0,0 +1,16 @@
+//
+//  XFormTests.swift
+//  XFormTests
+//
+//  Created by Jonathan Wight on 6/11/24.
+//
+
+import Testing
+
+struct XFormTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
