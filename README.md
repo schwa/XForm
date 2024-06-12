85e052fa09eeb0be2a79f494a680a2c7618d3c91
Jonathan Wight <jwight@mac.com>
2024-06-12 08:22:57 -0700

    Quick readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Description forthcoming
+# XForm
+
+An alternative "Form" API for SwiftUI highlighting Swift's new 'ForEach(subviewsOf:)' and 'Group(subViewsOf:)' APIs
+
+**further description forthcoming**
+
+```swift
+struct XForm <Content>: View where Content: View {
+    let content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack {
+                Group(subviewsOf: content) { subviews in
+                    ForEach(subviews.dropLast()) { subview in
+                        subview
+                        Divider()
+                    }
+                    subviews.last
+                }
+            }
+            .padding([.leading, .trailing])
+        }
+    }
+}
+```
+
+## Screenshot
+
+![alt text](<Documentation/Screenshot 2024-06-12 at 08.20.14.png>)
--- XForm/ContentView.swift
+++ XForm/ContentView.swift
@@ -6,81 +6,87 @@ struct ContentView: View {
     var name = "ContentView.swift"
 
     var body: some View {
-        XForm {
-            XSection("Transform") {
-                XSection("Scale") {
-                    TextField("x", value: .constant(0), format: .number)
-                        .fieldTitle("X")
-                    TextField("y", value: .constant(0), format: .number)
-                        .fieldTitle("X")
-                    TextField("z", value: .constant(0), format: .number)
-                        .fieldTitle("Y")
-                }
-                XSection("Rotation") {
-                    TextField("x", value: .constant(0), format: .number)
-                        .fieldTitle("X")
-                    TextField("y", value: .constant(0), format: .number)
-                        .fieldTitle("X")
-                    TextField("z", value: .constant(0), format: .number)
-                        .fieldTitle("Y")
-                }
-                XSection("Translation") {
-                    TextField("x", value: .constant(0), format: .number)
-                        .fieldTitle("X")
-                    TextField("y", value: .constant(0), format: .number)
-                        .fieldTitle("X")
-                    TextField("z", value: .constant(0), format: .number)
-                        .fieldTitle("Y")
+
+        ContentUnavailableView("This view intentionally left blank", systemImage: "gear")
+        .inspector(isPresented: .constant(true)) {
+            XForm {
+                XSection("Transform") {
+                    XSection("Scale") {
+                        TextField("x", value: .constant(0), format: .number)
+                            .fieldTitle("X")
+                        TextField("y", value: .constant(0), format: .number)
+                            .fieldTitle("X")
+                        TextField("z", value: .constant(0), format: .number)
+                            .fieldTitle("Y")
+                    }
+                    XSection("Rotation") {
+                        TextField("x", value: .constant(0), format: .number)
+                            .fieldTitle("X")
+                        TextField("y", value: .constant(0), format: .number)
+                            .fieldTitle("X")
+                        TextField("z", value: .constant(0), format: .number)
+                            .fieldTitle("Y")
+                    }
+                    XSection("Translation") {
+                        TextField("x", value: .constant(0), format: .number)
+                            .fieldTitle("X")
+                        TextField("y", value: .constant(0), format: .number)
+                            .fieldTitle("X")
+                        TextField("z", value: .constant(0), format: .number)
+                            .fieldTitle("Y")
+                    }
+
+                    XSection("Testing") {
+                        DateView()
+                    }
                 }
 
-                XSection("Testing") {
-                    DateView()
-                }
-            }
 
+                XSection("Identity and Type") {
+                    TextField("Name", text: $name)
+                        .fieldTitle("Name")
+                    TextField("Type", text: .constant("blah blah"))
+                        .fieldTitle("Type")
+                    TextField("Location", text: .constant("blah blah"))
+                        .fieldTitle("location")
+                    HStack {
+                        Text("ContentView.swift")
+                        Spacer()
+                        Image(systemName: "folder.fill")
+                    }
+                    .fieldLabel {
+                        Spacer()
+                    }
+                }
+                XSection("On Demand Resource Tags") {
+                    TextField("Name", text: .constant("Blah blah"))
+                }
+                XSection("Target Membership") {
+                    List {
+                        Label("MyApp", systemImage: "gear")
+                    }
+                    .frame(height: 80)
+                }
+                XSection("Text Settings") {
+                    Picker("Text Encoding", selection: .constant("utf8")) {
+                        Text("utf8").tag("utf8")
+                    }
+                    .fieldTitle("Text Encoding")
+                    Picker("Line Endings", selection: .constant("LF")) {
+                        Text("LF").tag("LF")
+                    }
+                    .fieldTitle("Line Endings")
+                    Divider()
+                    Picker("Indent Using", selection: .constant("Spaces")) {
+                        Text("Spaces").tag("Spaces")
+                    }
+                    .fieldTitle("Indent Using")
+                }
+            }
+            .controlSize(.mini)
 
-            XSection("Identity and Type") {
-                TextField("Name", text: $name)
-                    .fieldTitle("Name")
-                TextField("Type", text: .constant("blah blah"))
-                    .fieldTitle("Type")
-                TextField("Location", text: .constant("blah blah"))
-                    .fieldTitle("location")
-                HStack {
-                    Text("ContentView.swift")
-                    Spacer()
-                    Image(systemName: "folder.fill")
-                }
-                .fieldLabel {
-                    Spacer()
-                }
-            }
-            XSection("On Demand Resource Tags") {
-                TextField("Name", text: .constant("Blah blah"))
-            }
-            XSection("Target Membership") {
-                List {
-                    Label("MyApp", systemImage: "gear")
-                }
-                .frame(height: 80)
-            }
-            XSection("Text Settings") {
-                Picker("Text Encoding", selection: .constant("utf8")) {
-                    Text("utf8").tag("utf8")
-                }
-                .fieldTitle("Text Encoding")
-                Picker("Line Endings", selection: .constant("LF")) {
-                    Text("LF").tag("LF")
-                }
-                .fieldTitle("Line Endings")
-                Divider()
-                Picker("Indent Using", selection: .constant("Spaces")) {
-                    Text("Spaces").tag("Spaces")
-                }
-                .fieldTitle("Indent Using")
-            }
         }
-        .controlSize(.mini)
+
     }
 }
 
